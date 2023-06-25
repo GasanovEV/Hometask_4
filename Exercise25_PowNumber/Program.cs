279354d08766a2897f547d609148e77a765e275f
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-25 19:21:03 +0300

    Add Exercise25_PowNumber корректировка комментариев

--- Exercise25_PowNumber/Program.cs
+++ Exercise25_PowNumber/Program.cs
@@ -1,14 +1,15 @@
-﻿﻿//1.Input
+﻿﻿/* определяет переменные A и B через метод GetNumberFromUser*/
 int A = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода");
 int B = GetNumberFromUser("Введите целое число B: ", "Ошибка ввода");
 
-//2.Calculation
+//Возведение числа А в степень B
 double powNumbers = Math.Pow((double)A, (double)B);
 
-//3.Output
+//Выводит результат вычислений
 Console.WriteLine($"Число{A} возведенное в степень {B}  -> {powNumbers}");
 
-////////////////////////////////////
+/*Считывание введенного числа и передача его в переменную, при введении
+ не целочисленного значения выводится сообщение об ошибке*/
 
 int GetNumberFromUser(string message, string errorMessage)
 {
