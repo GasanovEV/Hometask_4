a7d75ac0d4dbc653cd9fe206f85102f6572ed8d6
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-25 19:05:32 +0300

    Add Exercise25_PowNumber

---
+++ Exercise25_PowNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿﻿//1.Input
+int A = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода");
+int B = GetNumberFromUser("Введите целое число B: ", "Ошибка ввода");
+
+//2.Calculation
+double powNumbers = Math.Pow((double)A, (double)B);
+
+//3.Output
+Console.WriteLine($"Число{A} возведенное в степень {B}  -> {powNumbers}");
+
+////////////////////////////////////
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(),out int userNumber))
+            return userNumber;
+        
+        Console.WriteLine(errorMessage);
+    }
+}
