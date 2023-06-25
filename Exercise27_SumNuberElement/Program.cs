0c28ed1f04f7c881c7d97fc7d4d23817f79a85ca
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-25 23:50:54 +0300

    Add Exercise27_SumNuberElement

---
+++ Exercise27_SumNuberElement/Program.cs
@@ -0,0 +1,31 @@
+﻿int number = GetNumberFromUser("Введите число: ", "Ошибка ввода");
+int sum = SumNumber (number);
+
+Console.WriteLine($"Сумма цифр числа {number}: -> {sum}");
+
+
+int SumNumber(int number)
+{
+    int sum = 0;
+    
+    while (number != 0)
+    {
+        int digit = number % 10;
+        sum += digit;
+        number /= 10;
+    }
+    return sum;
+}
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(),out int userNumber))
+            return userNumber;
+        
+        Console.WriteLine(errorMessage);
+    }
+}
